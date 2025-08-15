8e3d5d8a78771af0edbac142b0131eae7543e901
mokorCode <3091784019@qq.com>
2025-08-15 11:52:49 +0800

    commit

--- src/App.vue
+++ src/App.vue
@@ -3,22 +3,25 @@ import { globalData } from './utils/globalData';
 import Welcome from './views/Welcome/Welcome.vue';
 const global = globalData();
 import { gameListener } from './utils/emiter';
-import { onMounted } from 'vue';
+import { onMounted, ref } from 'vue';
 import _import from './assets/welcome/import';
+
+let preload = ref(false);
 
 onMounted(() => {
   document.title = 'wait...'
   _import().preload().then(() => {
     document.title = 'abc I love you'
     gameListener();
+    preload.value = true;
   });
 });
 </script>
 
 <template>
   <main>
-    <router-view v-if="global.gaming"></router-view>
-    <Welcome v-if="!global.gaming"></Welcome>
+    <router-view v-if="global.gaming && preloaded"></router-view>
+    <Welcome v-if="!global.gaming && preloaded"></Welcome>
   </main>
 </template>
 
