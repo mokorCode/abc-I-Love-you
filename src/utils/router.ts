fed31c4b7d642d615fb2bd094379075eeff840eb
mokorCode <3091784019@qq.com>
2025-08-15 09:49:52 +0800

    commit

--- src/utils/router.ts
+++ src/utils/router.ts
@@ -1,12 +1,11 @@
-import { createRouter, createWebHashHistory, createWebHistory } from "vue-router";
+import { createRouter, createWebHashHistory } from "vue-router";
 import Gaming from "@/views/Gaming/Gaming.vue";
 export const router = createRouter({
-    history: createWebHistory(),
-    routes: [
-        {
-            path: '/',
-            component: Gaming
-            }
-        ]
-    }
-)
+  history: createWebHashHistory(),
+  routes: [
+    {
+      path: "/",
+      component: Gaming,
+    },
+  ],
+});
